0defb95e0298bbce5b969a7afbf5b94963b6a443
PhillUndSoos <ph_hass@web.de>
2023-09-27 14:51:15 +0200

    entire (simple) store function

---
+++ store.js
@@ -0,0 +1,21 @@
+const createStore = (reducer) => {
+  let state; //initialising state and listeners inside createStore as to not pollute the global namespace
+  let listeners = [];
+
+  const getState = () => state; //returns the state
+
+  const dispatch = (action) => {
+    state = reducer(state, action); //invokes the reducer function, passing current state and action.
+    listeners.forEach((listener) => listener()); //Each listener that has been subscribed to the store (stored in the listeners array) is then notified of the change
+  };
+
+  const subscribe = (listener) => {
+    //adds the subscribe functionality
+    listeners.push(listener); //pushes each listener that into an array of listeners. (subscribing them)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener); //This can be used to unsubscribe listeners, removing them from the listeners array
+    };
+  };
+
+  return { getState, dispatch, subscribe }; //return an object containing all methods, allowing to interact with the store
+};
